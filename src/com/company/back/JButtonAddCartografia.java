cb9422ab779a774fb864185d659d43d955a269f6
otcz <54145021+otcz@users.noreply.github.com>
2025-06-02 17:06:21 -0500

    QUITAR EL OUT Y EL PRODUC

--- src/com/company/back/JButtonAddCartografia.java
+++ src/com/company/back/JButtonAddCartografia.java
@@ -18,108 +18,71 @@ import static com.company.ShowReadMeAddOn.getiLcyLucyEnv;
 
 public class JButtonAddCartografia extends JButton implements ActionListener {
 
+    private static final String RUTA_CAPA_COLOMBIA = "D:\\OTCZ\\Developer\\vulcano\\src\\cartografia\\capaalturacolombia";
+    private static final String RUTA_CAPA_MUNDO = System.getProperty("user.home") + "/Archivos Vulcano v2.0/cartografia/world";
+
+    private static final String LABEL_COLOMBIA = "Capa Colombia alturas";
+    private static final String LABEL_MUNDO = "Capa mundo";
+
     public JButtonAddCartografia() {
-        setIcon(new ImageIcon((getClass().getResource("mapareset.png"))));
+        setIcon(new ImageIcon(getClass().getResource("mapareset.png")));
         addActionListener(this);
-        setToolTipText("Puedes agregar cartografia predecargada (Capa Altura Colombia y Capa Mundo o World)");
-        File myDocuments = new File(System.getProperty("user.home") + "/" + "Archivos Vulcano v2.0");
+        setToolTipText("Puedes agregar cartografía precargada (Capa Altura Colombia y Capa Mundo)");
+
+        File myDocuments = new File(System.getProperty("user.home") + "/Archivos Vulcano v2.0");
         if (!myDocuments.exists()) {
             new CrearCarpetasVulcano();
         }
     }
 
-
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource().equals(this)) {
-            ILcdView vista = getiLcyLucyEnv().getCombinedMapManager().getActiveMapComponent().getMainView();
-            if (vista instanceof ILspView) {
-                try {
-                    File myDocuments = new File(System.getProperty("user.home") + "/" + "Archivos Vulcano v2.0");
-                    if (!myDocuments.exists()) {
-                        new CrearCarpetasVulcano();
-                    }
+        if (e.getSource() != this) return;
 
-                    isPosibleGraficarCapaColombia(colombia(System.getProperty("user.home") + "/" + "Archivos Vulcano v2.0/cartografia/capa altura colombia"));
-                    isPosibleGraficarCapaMundo(capaMundo(System.getProperty("user.home") + "/" + "Archivos Vulcano v2.0/cartografia/world"));
-
-                } catch (Exception n) {
-                    n.printStackTrace();
-                }
-
-            }
-        }
-
-    }
-
-    public ILspLayer colombia(String ruta) {
-        TLcdEarthRepositoryModelDecoder decoder = new TLcdEarthRepositoryModelDecoder();
-        ILspLayer layer = null;
-        try {
-            ILcdModel model = decoder.decode(ruta);
-            BasicLayerFactory factory = new BasicLayerFactory();
-            layer = factory.createLayer(model);
-            layer.setLabel("Capa Colombia alturas");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return layer;
-
-    }
-
-    public ILspLayer capaMundo(String ruta) {
-
-        TLcdEarthRepositoryModelDecoder decoder = new TLcdEarthRepositoryModelDecoder();
-        ILspLayer layer = null;
+        ILspView vista3D = obtenerVista3D();
+        if (vista3D == null) return;
 
         try {
-            ILcdModel model = decoder.decode(ruta);
-
-            BasicLayerFactory factory = new BasicLayerFactory();
-            layer = factory.createLayer(model);
-            layer.setLabel("Capa mundo");
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            agregarCapaSiNoExiste(vista3D, RUTA_CAPA_COLOMBIA, LABEL_COLOMBIA);
+            agregarCapaSiNoExiste(vista3D, RUTA_CAPA_MUNDO, LABEL_MUNDO);
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
+    }
+
+    private ILspView obtenerVista3D() {
+        ILcdView vista = getiLcyLucyEnv().getCombinedMapManager().getActiveMapComponent().getMainView();
+        return (vista instanceof ILspView) ? (ILspView) vista : null;
+    }
+
+    private void agregarCapaSiNoExiste(ILspView vista3D, String ruta, String label) throws IOException {
+        if (yaExisteCapa(vista3D, label)) return;
+
+        ILspLayer capa = crearCapaDesdeRuta(ruta, label);
+        if (capa != null) {
+            vista3D.addLayer(capa);
+            if (label.equals(LABEL_COLOMBIA)) {
+                vista3D.moveLayerAt(vista3D.layerCount() - 1, capa); // mover al fondo
+            }
+        }
+    }
+
+    private boolean yaExisteCapa(ILspView vista3D, String label) {
+        for (int i = 0; i < vista3D.layerCount(); i++) {
+            String capaLabel = vista3D.getLayer(i).getLabel();
+            if (capaLabel.equalsIgnoreCase(label) || (label.equals(LABEL_MUNDO) && capaLabel.equalsIgnoreCase("World"))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private ILspLayer crearCapaDesdeRuta(String ruta, String label) throws IOException {
+        TLcdEarthRepositoryModelDecoder decoder = new TLcdEarthRepositoryModelDecoder();
+        ILcdModel model = decoder.decode(ruta);
+        BasicLayerFactory factory = new BasicLayerFactory();
+        ILspLayer layer = factory.createLayer(model);
+        layer.setLabel(label);
         return layer;
-
     }
-
-    private boolean isPosibleGraficarCapaMundo(ILspLayer layer) {
-        ILcdView vista = getiLcyLucyEnv().getCombinedMapManager().getActiveMapComponent().getMainView();
-        ILspView vista3D = null;
-        if (vista instanceof ILspView) {
-            vista3D = (ILspView) vista;
-            for (int i = 0; i < vista3D.layerCount(); i++) {
-                if (vista3D.getLayer(i).getLabel().equals("Capa mundo") | vista3D.getLayer(i).getLabel().equals("World")) {
-                    return false;
-                }
-            }
-
-        }
-        vista3D.addLayer(layer);
-        return true;
-    }
-
-    private boolean isPosibleGraficarCapaColombia(ILspLayer layer) {
-        ILcdView vista = getiLcyLucyEnv().getCombinedMapManager().getActiveMapComponent().getMainView();
-        ILspView vista3D = null;
-        if (vista instanceof ILspView) {
-            vista3D = (ILspView) vista;
-            for (int i = 0; i < vista3D.layerCount(); i++) {
-                if (vista3D.getLayer(i).getLabel().equals("Capa Colombia alturas")) {
-                    return false;
-                }
-            }
-
-        }
-        vista3D.addLayer(layer);
-        vista3D.moveLayerAt(vista3D.layerCount() - 1, layer);
-        return true;
-    }
-
-
 }
