641c2cf3325fb5fdaa508eb4ab80c025fcf73210
otcz <54145021+otcz@users.noreply.github.com>
2025-06-02 23:11:54 -0500

    FULL XML

--- build/project/obfuscate_project.bat
+++ build/project/obfuscate_project.bat
@@ -3,6 +3,6 @@ echo Please read the comments in the obfuscate_project.xml file.
 
 set ANT_HOME=../ant
 set ANT_OPTS=-Xmx1024m
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_181\
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_202\
 
 ..\ant\bin\ant -buildfile obfuscate_project2.xml %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ lib/lightspeed/Vulcano.bat
@@ -0,0 +1,2 @@
+@Echo Off
+java -jar VulcanoCAFUE.jar
